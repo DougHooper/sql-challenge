765d298bf1c8efdc7f123604f120b0db47a03ffe
Doug Hooper <hooprds@gmail.com>
2023-05-06 17:13:30 -0400

    updated sql docs

---
+++ challengeschema.sql
@@ -0,0 +1,50 @@
+--select * from titles;
+
+--create table employees ( 
+--	emp_no INT primary key,
+--	emp_title_id VARCHAR,
+--	birth_date DATE,
+--	first_name VARCHAR,
+--	last_name VARCHAR,
+--	sex VARCHAR, 
+--	hire_date DATE,
+--	foreign key (emp_title_id) references titles (title_id)
+--);
+
+--select * from employees;
+
+--create table departments (
+--	dept_no VARCHAR Primary Key,
+--	dept_name VARCHAR
+--);
+
+--select * from departments;
+
+--create table dept_manager(
+--	dept_no VARCHAR,
+--	emp_no INT,
+--	foreign key (emp_no) references employees (emp_no),
+--	foreign key (dept_no) references departments (dept_no),
+--	primary key (dept_no, emp_no)
+--);
+
+--select * from dept_manager; 
+
+--create table dept_emp(
+--	emp_no INT,
+--	dept_no VARCHAR,
+--	foreign key (emp_no) references employees (emp_no),
+--	foreign key (dept_no) references departments (dept_no),
+--	primary key (emp_no, dept_no)
+--);
+
+--select * from dept_emp;
+
+--create table salaries (
+--	emp_no INT primary key,
+--	salary INT,
+--	foreign key (emp_no) references employees (emp_no)
+--)
+
+--select * from salaries;
+
